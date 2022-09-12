388c5fca7df4b75b5d8f5c970fefb6612e153cde
Jose Chavarria <jose.chavarria@bg.com.bo>
2022-09-12 10:15:52 -0400

    apk firmada

--- android/app/src/main/java/com/bg/ganamovil/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/bg/ganamovil/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.awesomeproject.newarchitecture.modules;
+package com.bg.ganamovil.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
